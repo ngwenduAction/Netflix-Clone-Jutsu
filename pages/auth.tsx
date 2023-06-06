9a6c0ba11fb879757d68d073d726e95d28b01ba9
Ngwendu <gambungwendu@gmail.com>
2023-06-06 16:46:18 +0200

    installed axios

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -14,6 +14,8 @@ const Auth = () => {
     );
   }, []);
 
+  const register = useCallback(async () => {}, []);
+
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
