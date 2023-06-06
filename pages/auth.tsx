0e2651da571064a0eb93b09b4f2be2f7751dd313
Ngwendu <gambungwendu@gmail.com>
2023-06-06 19:03:27 +0200

    created register file with imports

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useCallback, useState } from "react";
 import Input from "@/components/Input";
 
@@ -14,7 +15,17 @@ const Auth = () => {
     );
   }, []);
 
-  const register = useCallback(async () => {}, []);
+  const register = useCallback(async () => {
+    try {
+      await axios.post("/api/register", {
+        email,
+        name,
+        password,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
