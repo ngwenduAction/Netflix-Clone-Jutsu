0da8a0cc26d82918d0565a037524d991ba059bea
Ngwendu <gambungwendu@gmail.com>
2023-06-07 10:28:32 +0200

    onClick register for now.

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -25,6 +25,7 @@ const Auth = () => {
     } catch (error) {
       console.log(error);
     }
+    // dependency array
   }, [email, name, password]);
 
   return (
@@ -62,7 +63,10 @@ const Auth = () => {
                 value={password}
               />
             </div>
-            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+            <button
+              onClick={register}
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+            >
               {variant === "login" ? "Login" : "Sign up"}
             </button>
             <p className="text-neutral-500 mt-12">
