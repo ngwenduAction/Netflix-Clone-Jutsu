9f6ae0b8b34329a54ff291ac19acfd9f0b2afffd
Ngwendu <gambungwendu@gmail.com>
2023-06-08 21:03:04 +0200

    two types of providers added

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,10 +2,17 @@ import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import { compare } from 'bcrypt';
 
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+
 import prismadb from '@/lib/prismadb';
 
 export default NextAuth({
     providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID || '',
+            clientSecret: process.env.GITHUB_SECRET || ''
+        }),
         Credentials({
            id: 'credentials',
            name: 'Credentials',
