fe1a93f2d72213d64754548265b8fff824811b28
Ngwendu <gambungwendu@gmail.com>
2023-06-08 21:40:34 +0200

    google and github providers plus @next-auth/prisma-adapter installation

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -13,6 +13,10 @@ export default NextAuth({
             clientId: process.env.GITHUB_ID || '',
             clientSecret: process.env.GITHUB_SECRET || ''
         }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID || '',
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
+        }),
         Credentials({
            id: 'credentials',
            name: 'Credentials',
