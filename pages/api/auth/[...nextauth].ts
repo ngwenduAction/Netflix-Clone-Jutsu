b883a65bd0bd471463d946572e997acbed8475c0
Ngwendu <gambungwendu@gmail.com>
2023-06-08 21:47:11 +0200

    import PrismaAdapter

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,6 +5,8 @@ import { compare } from 'bcrypt';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+
 import prismadb from '@/lib/prismadb';
 
 export default NextAuth({
