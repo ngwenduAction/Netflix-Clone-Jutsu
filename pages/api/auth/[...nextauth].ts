25bfcc1da15c2994ed965ffa093bd421a3caea39
Ngwendu <gambungwendu@gmail.com>
2023-06-11 16:49:12 +0200

    github authentication ids

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -64,6 +64,7 @@ export default NextAuth({
         signIn: '/auth',
     },
     debug: process.env.NODE_ENV === 'development',
+    adapter: PrismaAdapter(prismadb),
     session: {
         strategy: 'jwt',
     },
