3dcace6a2797ef14fa5281465ce0286cf72f0ee0
Ngwendu <gambungwendu@gmail.com>
2023-07-03 23:56:51 +0200

    create account menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,16 @@
+import { useCallback, useState } from "react";
+import { BsChevronDown, BsSearch, BsBell } from "react-icons/bs";
+
 import NavbarItem from "./NavbarItem";
+import MobileMenu from "./MobileMenu";
 
 const Navbar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+  const toggleMobileMenu = useCallback(() => {
+    setShowMobileMenu((current) => !current);
+  }, []);
+
   return (
     <nav className="w-full fixed z-40">
       <div
@@ -27,7 +37,34 @@ const Navbar = () => {
             lg:flex
           "
         >
-          <NavbarItem />
+          <NavbarItem label="Home" />
+          <NavbarItem label="TV Shows" />
+          <NavbarItem label="Movies" />
+          <NavbarItem label="New & Popular" />
+          <NavbarItem label="My List" />
+          <NavbarItem label="Browse by Languages" />
+        </div>
+        <div
+          onClick={toggleMobileMenu}
+          className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative"
+        >
+          <p className="text-white text-sm">Browse</p>
+          <BsChevronDown className="text-white transition" />
+          <MobileMenu visible={showMobileMenu} />
+        </div>
+        <div className="flex flex-row ml-auto gap-7 items-center">
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
+            <BsSearch />
+          </div>
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
+            <BsBell />
+          </div>
+          <div className="flex flex-row items-center gap-2 cursor-pointer relative">
+            <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
+              <img src="/images/default-blue.png" alt="" />
+            </div>
+            <BsChevronDown className="text-white transition" />
+          </div>
         </div>
       </div>
     </nav>
--- components/NavbarItem.tsx
+++ components/NavbarItem.tsx
@@ -1,5 +1,15 @@
-const NavbarItem = () => {
-  return <div></div>;
+import React from "react";
+
+interface NavbarItemProps {
+  label: string;
+}
+
+const NavbarItem: React.FC<NavbarItemProps> = ({ label }) => {
+  return (
+    <div className="text-white cursor-pointer hover:text-gray-300 transition">
+      {label}
+    </div>
+  );
 };
 
 export default NavbarItem;
