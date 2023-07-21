78b9ecb5839cb79b755a7c9296d419ca7110593c
Ngwendu <gambungwendu@gmail.com>
2023-07-21 17:16:35 +0200

    favorite button

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useCallback, useMemo } from "react";
+import { AiOutlinePlus } from "react-icons/ai";
 
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useFavorites from "@/hooks/useFavorites";
@@ -9,7 +10,28 @@ interface FavoriteButtonProps {
 }
 
 const FavoriteButton: React.FC<FavoriteButtonProps> = ({ movieId }) => {
-  return <div></div>;
+  return (
+    <div
+      className="
+        cursor-pointer
+        group/item
+        w-6
+        h-6
+        lg:w-10
+        lg:h-10
+        border-white
+        border-2
+        rounded-full
+        flex
+        justify-center
+        items-center
+        transition
+        hover:border-neutral-300
+     "
+    >
+      <AiOutlinePlus className="text-white" size={25} />
+    </div>
+  );
 };
 
 export default FavoriteButton;
--- pages/index.tsx
+++ pages/index.tsx
@@ -32,6 +32,7 @@ export default function Home() {
       <Billboard />
       <div className="pb-40">
         <MovieList title="Trending Now" data={movies} />
+        <MovieList title="My List" data={movies} />
       </div>
     </>
   );
