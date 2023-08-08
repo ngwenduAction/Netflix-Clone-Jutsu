3bcaa16d2670eda473119020dd9d11ee62bcd4a7
Ngwendu <gambungwendu@gmail.com>
2023-08-08 10:06:31 +0200

    ups

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -29,7 +29,7 @@ const Profiles = () => {
     <div className="flex items-center h-full justify-center">
       <div className="flex flex-col">
         <h1 className="text-3xl md:text-6xl text-white text-center">
-          Who's watching?
+          Who&rsquo;s watching?
         </h1>
         <div className="flex items-center justify-center gap-8 mt-10">
           <div onClick={() => router.push("/")}>
