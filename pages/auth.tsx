d435d7a07f626e587e079b6fd43597431a9d066b
Ngwendu <gambungwendu@gmail.com>
2023-08-13 10:55:07 +0200

    updates

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -4,7 +4,6 @@ import { signIn } from "next-auth/react";
 
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
-import Image from "next/image";
 
 import Input from "@/components/Input";
 
@@ -53,14 +52,7 @@ const Auth = () => {
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
         <nav className="px-12 py-5">
-          {/* Using specific dimensions (pixels) */}
-          <Image
-            src="/images/logo.png"
-            width={1280}
-            height={346}
-            alt="Logo"
-            className="h-12"
-          />
+          <img src="/images/logo.png" alt="Logo" className="h-12" />
         </nav>
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
